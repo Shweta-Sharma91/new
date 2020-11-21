4fc1f8b9cdbe8ff029d7befdaec5586947010f75
Shweta-Sharma91 <shwetasharmagarg91@gmail.com>
2020-11-21 16:59:49 +0530

    object practise programs

---
+++ javascript code/object conditionals qustion.js
@@ -0,0 +1,10 @@
+var laptop={
+	name:"dell",
+	color:"black",
+	ram:"8gb"
+};
+if(laptop.name!="apple")
+{
+	laptop.name="apple"
+}
+console.log(laptop)
---
+++ javascript code/object.js
@@ -0,0 +1,14 @@
+var samsung={
+	model_number: "SM-A507FN/DS",
+	serial_number: "RZ8M90ME9BP",
+	number_of_apps: 97,
+	phone_color: "white",
+	name: "Galaxy A50s",
+	whatsapp: "yes",
+	instagram: "no"
+};
+console.log(samsung)
+console.table(samsung)
+console.log(samsung.name)
+samsung.name="Galaxy J7"
+console.log(samsung.name)
