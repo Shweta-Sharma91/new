4fad9be94e38f6cb8484d39a8fe0e6591926eb26
Shweta-Sharma91 <shwetasharmagarg91@gmail.com>
2020-11-22 22:22:02 +0530

    foreach/for in or of loop and one html file uploading

---
+++ javascript code/for each in javascript.js
@@ -0,0 +1,5 @@
+/*var name_array=["shweta","priya","ritu"]
+name_array.forEach((n) => (console.log(n)))*/
+
+var number_array=[1,2,3,4]
+number_array.forEach((num) => (console.log(num+1)))
---
+++ javascript code/for in or for of .js
@@ -0,0 +1,8 @@
+var laptop={ 
+name:"dell",
+color:"white",
+ram:"8Gb"
+};
+for(var n in laptop){
+	console.log(laptop[n])
+}
